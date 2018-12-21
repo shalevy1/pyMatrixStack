2ca9fd16568b7217e4fe34257c65b89df7c23f3e
William Emerison Six <billsix@gmail.com>
2018-12-20 20:28:48 -0500

    initial commit, extracted from pynuklear

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup, Extension
+
+
+setup (name = 'pyMatrixStack',
+       version = '0.0.1',
+       description = 'Matrix stack for OpenGL',
+       author = 'William Emerison Six',
+       author_email = 'billsix@gmail.com',
+       url = 'https://github.com/billsix/pyMatrixStack',
+       keywords = "nuklear imgui",
+       license = "MIT",
+       packages=['pyMatrixStack'],
+       package_dir={'pyMatrixStack': 'src/pyMatrixStack'},
+       classifiers=[
+           "Development Status :: 3 - Alpha",
+           "Topic :: Utilities",
+           "License :: OSI Approved :: MIT License",
+       ],
+       long_description = '''
+A matrix stack implementation for OpenGL 3+
+''')
